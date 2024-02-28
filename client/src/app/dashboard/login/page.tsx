ca57b31c6478fadd035219176df5c1b378f0e772
Kyle Hu <lloydpearce4@gmail.com>
2024-02-28 02:10:45 -0800

    sortof implemented login w google

--- client/src/app/dashboard/login/page.tsx
+++ client/src/app/dashboard/login/page.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+"use client";
+
+// Components
 import LoginForm from "@/components/LoginForm/LoginForm";
+
+import { loginWithGoogle } from "@/utils/supabase/server-actions";
 
 const page = () => {
     return (
         <>
             <LoginForm />
+            {/* prettier-ignore */}
+            <button onClick={() => {loginWithGoogle()}}>Hope this works</button>
         </>
     );
 };
--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -2,23 +2,12 @@
 
 import { useToggle, upperFirst } from "@mantine/hooks";
 import { useForm } from "@mantine/form";
-import {
-    TextInput,
-    PasswordInput,
-    Text,
-    Paper,
-    Group,
-    PaperProps,
-    Button,
-    Checkbox,
-    Anchor,
-    Stack,
-    Center,
-} from "@mantine/core";
+import { Text, Paper, Center } from "@mantine/core";
 
 import classes from "./LoginForm.module.css";
+import { GoogleButton } from "../GoogleButton/GoogleButton";
 
-function LoginForm(props: PaperProps) {
+function LoginForm() {
     const [type, toggle] = useToggle(["login", "register"]);
     const form = useForm({
         initialValues: {
@@ -44,80 +33,12 @@ function LoginForm(props: PaperProps) {
                 p="xl"
                 withBorder
             >
-                <Text size="lg" fw={500}>
-                    Welcome to JobTracker, {type} with
+                {/* prettier-ignore */}
+                <Text size="lg" fw={500} classNames={{root: classes.textRoot}}>
+                    Welcome to <Text span variant="gradient" gradient={{ from: 'indigo.4', to: 'violet' }}>JobTracker</Text> {type} with
                 </Text>
-
-                <form
-                    onSubmit={form.onSubmit((values) =>
-                        console.log(values, type),
-                    )}
-                >
-                    <Stack>
-                        <TextInput
-                            required
-                            label="Email"
-                            placeholder="hello@mantine.dev"
-                            value={form.values.email}
-                            onChange={(event) =>
-                                form.setFieldValue(
-                                    "email",
-                                    event.currentTarget.value,
-                                )
-                            }
-                            error={form.errors.email && "Invalid email"}
-                            radius="md"
-                        />
-
-                        <PasswordInput
-                            required
-                            label="Password"
-                            placeholder="Your password"
-                            value={form.values.password}
-                            onChange={(event) =>
-                                form.setFieldValue(
-                                    "password",
-                                    event.currentTarget.value,
-                                )
-                            }
-                            error={
-                                form.errors.password &&
-                                "Password should include at least 6 characters"
-                            }
-                            radius="md"
-                        />
-
-                        {type === "register" && (
-                            <Checkbox
-                                label="I accept terms and conditions"
-                                checked={form.values.terms}
-                                onChange={(event) =>
-                                    form.setFieldValue(
-                                        "terms",
-                                        event.currentTarget.checked,
-                                    )
-                                }
-                            />
-                        )}
-                    </Stack>
-
-                    <Group justify="space-between" mt="xl">
-                        <Anchor
-                            component="button"
-                            type="button"
-                            c="dimmed"
-                            onClick={() => toggle()}
-                            size="xs"
-                        >
-                            {type === "register"
-                                ? "Already have an account? Login"
-                                : "Don't have an account? Register"}
-                        </Anchor>
-                        <Button type="submit" radius="xl">
-                            {upperFirst(type)}
-                        </Button>
-                    </Group>
-                </form>
+                {/* prettier-ignore */}
+                <GoogleButton onClick={() => {console.log("Hello")}} radius="xl">Google</GoogleButton>
             </Paper>
         </Center>
     );

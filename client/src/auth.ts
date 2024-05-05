4a3a756bac371e32f28f1c7901847a0894ee9732
Kyle Hu <lloydpearce4@gmail.com>
2024-05-04 21:32:14 -0700

    Auth callback

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -3,4 +3,12 @@ import google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	providers: [google],
+
+	// We use a callback here to create a record for the user in supabase
+	callbacks: {
+		async signIn({ user, account, profile, email, credentials }) {
+			console.log("Success signin");
+			return true;
+		},
+	},
 });

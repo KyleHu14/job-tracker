f9457bc184d8247724f4bea8a0d82ef92dc18715
Kyle Hu <lloydpearce4@gmail.com>
2024-05-14 01:07:41 -0700

    Updated navbar so that it can sign a user out

--- client/src/components/DashboardNavbar.tsx
+++ client/src/components/DashboardNavbar.tsx
@@ -1,14 +1,64 @@
+import { signOut } from "@/auth";
 import { auth } from "../auth";
+
+import {
+	Menubar,
+	MenubarContent,
+	MenubarItem,
+	MenubarMenu,
+	MenubarSeparator,
+	MenubarTrigger,
+} from "@/components/ui/menubar";
 
 export default async function DashboardNavbar() {
 	const session = await auth();
 
-	if (!session?.user) return null;
+	if (!session?.user || !session?.user.image) return null;
 
 	return (
-		<nav className="flex justify-between p-5 border-b-2">
+		<nav className="flex justify-between items-center px-5 py-2 border-b-2">
 			<a href="/dashboard">JobTracker</a>
-			<div>{session.user.name}</div>
+			{/* User Drop Down Menu */}
+			<Menubar>
+				<MenubarMenu>
+					<MenubarTrigger>
+						<img
+							className="w-10 h-10 rounded-full cursor-pointer"
+							src={session.user.image}
+							alt="User Avatar"
+						/>
+					</MenubarTrigger>
+					<MenubarContent>
+						{/* User Information */}
+						<div className="flex flex-col mt-1.5">
+							<div className="px-2 text-[0.95rem]">
+								{session.user.name}
+							</div>
+							<div className="px-2 text-[0.8rem] text-gray-500">
+								{session.user.email}
+							</div>
+						</div>
+
+						{/* Line */}
+						<MenubarSeparator />
+
+						{/* Sign Out Button */}
+
+						<form
+							action={async () => {
+								"use server";
+								console.log("Trigger");
+								await signOut({
+									redirect: true,
+									redirectTo: "/",
+								});
+							}}
+							className="w-full">
+							<button>Sign Out</button>
+						</form>
+					</MenubarContent>
+				</MenubarMenu>
+			</Menubar>
 		</nav>
 	);
 }

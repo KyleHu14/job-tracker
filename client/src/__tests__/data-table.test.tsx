0271d424a98f60be238056e447272394e595cc8f
Lloyd <lloydpearce4@gmail.com>
2024-05-29 13:57:32 -0700

    Started to add tests in client

---
+++ client/src/__tests__/data-table.test.tsx
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest"
+
+import { render, screen } from "@testing-library/react"
+
+import { DataTable } from "@/app/dashboard/data-table"
+
+test("Data Table renders content correctly", () => {})
